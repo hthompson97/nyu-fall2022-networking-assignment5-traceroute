ac7699c7d07402036be65abe533b6de9365b908a
Hannah Thompson <35496083+hthompson97@users.noreply.github.com>
2022-04-05 20:58:17 -0400

    ttl as string

--- solution.py
+++ solution.py
@@ -98,7 +98,7 @@ def get_route(hostname):
                 howLongInSelect = (time.time() - startedSelect)
                 if whatReady[0] == []: # Timeout
                     #tracelist1.append("  %d    *    Request timed out"%(ttl))
-                    tracelist1 = [ttl,"*","Request timed out"]
+                    tracelist1 = [str(ttl),"*","Request timed out"]
                     ############################################TO REMOVE############################################
                     print("  %d    *    Request timed out"%(ttl))
                     ############################################TO REMOVE############################################
@@ -112,7 +112,7 @@ def get_route(hostname):
                 timeLeft = timeLeft - howLongInSelect
                 if timeLeft <= 0:
                     #tracelist1.append("  %d    *    Request timed out"%(ttl))
-                    tracelist1 = [ttl,"*","Request timed out"]
+                    tracelist1 = [str(ttl),"*","Request timed out"]
                     ############################################TO REMOVE############################################
                     print("  %d    *    Request timed out"%(ttl))
                     ############################################TO REMOVE############################################
@@ -149,7 +149,7 @@ def get_route(hostname):
 
                     #tracelist1.append("  %d    %.0f ms    %s %s" %(ttl, (timeReceived -t)*1000, addr[0], "hostname not returnable")) #“hostname not returnable”.
                     time_formatted = "%.0fms"%((timeReceived -t)*1000)
-                    tracelist1 = [ttl,time_formatted,"hostname not returnable"]
+                    tracelist1 = [str(ttl),time_formatted,"hostname not returnable"]
                     tracelist2.append(tracelist1)
                     continue
                     #Fill in end
@@ -166,7 +166,7 @@ def get_route(hostname):
 
                     #tracelist1.append("  %d    %.0f ms    %s %s" %(ttl, (timeReceived -t)*1000, addr[0], recvaddr))
                     time_formatted = "%.0fms"%((timeReceived -t)*1000)
-                    tracelist1 = [ttl,time_formatted, addr[0], recvaddr]
+                    tracelist1 = [str(ttl),time_formatted, addr[0], recvaddr]
                     tracelist2.append(tracelist1)
                     #Fill in end
                 elif types == 3:#unreachable
@@ -176,7 +176,7 @@ def get_route(hostname):
                     #You should add your responses to your lists here
                     #tracelist1.append(" %d   %.0f ms %s %s" % (ttl,(timeReceived -t)*1000, addr[0], recvaddr))
                     time_formatted = "%.0fms"%((timeReceived -t)*1000)
-                    tracelist1 = [ttl,time_formatted, addr[0], recvaddr]
+                    tracelist1 = [str(ttl),time_formatted, addr[0], recvaddr]
                     tracelist2.append(tracelist1)
                     ############################################TO REMOVE############################################
                     print("  %d    %.0f ms    %s %s" %(ttl, (timeReceived -t)*1000, addr[0], recvaddr))
@@ -187,7 +187,7 @@ def get_route(hostname):
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     time_formatted = "%.0fms"%((timeReceived -t)*1000)
-                    tracelist1 = [ttl,time_formatted, addr[0], recvaddr]
+                    tracelist1 = [str(ttl),time_formatted, addr[0], recvaddr]
                     #tracelist1.append("  %d    %.0f ms    %s %s" % (ttl,(timeReceived -timeSent)*1000, addr[0], recvaddr))
 
                     #Fill in start
